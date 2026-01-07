996aa0bd24d8d74c222e97025554f921c06b8cf0
ariel elhayani <89216957+arielushka@users.noreply.github.com>
2026-01-07 16:00:01 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -254,4 +254,28 @@ public class test
         }
         return maxSum;
     }
+    public static void change(Stack<Integer> s)
+    {
+    	Queue<Integer> q = new Queue<>();
+    	while(!s.isEmpty())
+    	{
+    		q.insert(s.pop());
+    	}
+    	while(!q.isEmpty())
+    	{
+    		s.push(q.remove());
+    	}
+    }
+    public static void change(Queue<Integer> q)
+    {
+    	Stack<Integer> s = new Stack<>();
+    	while(!q.isEmpty())
+    	{
+    		s.push(q.remove());
+    	}
+    	while(!s.isEmpty())
+    	{
+    		q.insert(s.pop());
+    	}
+    }
 }
