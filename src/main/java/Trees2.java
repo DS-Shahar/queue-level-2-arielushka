374ee49c7dfdfeb95ff692360072e8f9c29d8368
ariel elhayani <89216957+arielushka@users.noreply.github.com>
2026-02-01 11:19:13 +0200

    Refactor tree functions and update method names

--- src/main/java/Trees2.java
+++ src/main/java/Trees2.java
@@ -13,10 +13,8 @@ public class Trees2 {
         BinNode<Integer> n1 = new BinNode<>(10);
         BinNode<Integer> n2 = new BinNode<>(-5);
         BinNode<Integer> n3 = new BinNode<>(20);
-        BinNode<Integer> n4 = new BinNode<>(-10);
         n1.setLeft(n2);
         n1.setRight(n3);
-        n2.setLeft(n4);
         return n1;
     }
 
@@ -36,34 +34,42 @@ public class Trees2 {
     public static boolean ex18(BinNode<Integer> t1, BinNode<Integer> t2) {
         if (t2 == null)
             return true;
-        if (!isExists(t1, t2.getValue()))
+        if (!contains(t1, t2.getValue()))
             return false;
         return ex18(t1, t2.getLeft()) && ex18(t1, t2.getRight());
     }
 
-    private static boolean isExists(BinNode<Integer> t, int val) {
+    public static boolean contains(BinNode<Integer> t, int val) {
         if (t == null)
             return false;
         if (t.getValue() == val)
             return true;
-        return isExists(t.getLeft(), val) || isExists(t.getRight(), val);
+        return contains(t.getLeft(), val) || contains(t.getRight(), val);
     }
 
     public static int ex20(BinNode<Integer> t) {
+        if (t == null)
+            return 0;
         return sumPos(t) - Math.abs(sumNeg(t));
     }
 
-    private static int sumPos(BinNode<Integer> t) {
+    public static int sumPos(BinNode<Integer> t) {
         if (t == null)
             return 0;
-        int current = (t.getValue() > 0) ? t.getValue() : 0;
+        int current = 0;
+        if (t.getValue() > 0) {
+            current = t.getValue();
+        }
         return current + sumPos(t.getLeft()) + sumPos(t.getRight());
     }
 
-    private static int sumNeg(BinNode<Integer> t) {
+    public static int sumNeg(BinNode<Integer> t) {
         if (t == null)
             return 0;
-        int current = (t.getValue() < 0) ? t.getValue() : 0;
+        int current = 0;
+        if (t.getValue() < 0) {
+            current = t.getValue();
+        }
         return current + sumNeg(t.getLeft()) + sumNeg(t.getRight());
     }
 }
